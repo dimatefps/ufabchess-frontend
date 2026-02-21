f86b6bdfe70c2ed643fe1095e8d16b8a675f3410
Mate <matheusryan406@gmail.com>
2026-02-21 00:39:42 -0300

    feat: implementação de segurança RLS

--- docs/admin/scripts/supabase.js
+++ docs/admin/scripts/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://wnhvujnfkzsbqadjghng.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduaHZ1am5ma3pzYnFhZGpnaG5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MzgwMTksImV4cCI6MjA4NTAxNDAxOX0.9xAWVisUcT0pLKSOv-KIp9ahujjJ0_Vdv9WyD73H030";
+const SUPABASE_ANON_KEY = "sb_publishable_p2p_rTFA4X18FysJZgXNKQ_y--Wexe4";
 
 export const supabase = createClient(
   SUPABASE_URL,

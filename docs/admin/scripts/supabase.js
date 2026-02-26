8b1f3a5413e8ffeb6d568297b976af80a51a9f47
Gsimarelli <gsimarelli@ymail.com>
2026-02-26 19:40:11 -0300

    desculpa mate

--- docs/admin/scripts/supabase.js
+++ docs/admin/scripts/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const ENV = {
-  SUPABASE_URL: "https://wnhvujnfkzsbqadjghng.supabase.co/",
+  SUPABASE_URL: "https://wnhvujnfkzsbqadjghng.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_p2p_rTFA4X18FysJZgXNKQ_y--Wexe4"
 };
 
